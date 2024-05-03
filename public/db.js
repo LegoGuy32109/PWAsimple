ba152a173c79f8fb88fdc116aeacbe34477f4d01
LegoGuy32109 <joshevanhale@gmail.com>
2024-05-03 16:19:37 -0400

    auto refresh and random gen

--- public/db.js
+++ public/db.js
@@ -1,119 +1,137 @@
-const customerDatabase = {
-  db: null,
-  startData: [
-    { ssn: "444-44-4444", name: "Bill", age: 35, email: "bill@company.com" },
-    { ssn: "555-55-5555", name: "Donna", age: 32, email: "donna@home.org" },
-  ],
+const startData = [
+	{ ssn: "444-44-4444", name: "Bill", age: 35, email: "bill@company.com" },
+	{ ssn: "555-55-5555", name: "Donna", age: 32, email: "donna@home.org" },
+	{ ssn: "666-66-6666", name: "William", age: 42, email: "william@swag.edu" },
+];
 
-  /**
-   * Get db object save it to `db`
-   */
-  getDb() {
-    console.log("Connecting to DB");
-    const request = window.indexedDB.open("TestDB", 1);
-    request.onerror = (err) => {
-      console.error(err);
-    };
-    request.onsuccess = (event) => {
-      this.db = event.target.result;
-      console.log("Db opened ✅", event);
-    };
-    request.onupgradeneeded = (event) => {
-      console.log("Upgrading DB", event);
-      const db = event.target.result;
-      const newStore = db.createObjectStore("customers", {
-        autoIncrement: true,
-      });
-      // not running for some reason
-      newStore.onsuccess = async (event) => {
-        console.log("Customers store created", event);
-        console.log("Creating customers 🧑👩");
-        for (const customer of this.startData) {
-          console.log("🧑 Adding ", customer);
-          await newStore.add(customer);
-        }
-      };
-    };
-  },
+const appDatabase = {
+	// The IndexedDb object
+	db: null,
+
+	/**
+	 * Get db object save it to `db`
+	 */
+	connectDb() {
+		return new Promise((resolve, reject) => {
+			const request = window.indexedDB.open("TestDB", 1);
+			request.onerror = (err) => {
+				reject(err);
+			};
+			request.onsuccess = (event) => {
+				this.db = event.target.result;
+				resolve(event);
+			};
+			request.onupgradeneeded = (event) => {
+				const db = event.target.result;
+				db.createObjectStore("customers", { autoIncrement: true });
+			};
+		});
+	},
+
+	/**
+	 * Make customers table empty
+	 */
+	clearDb() {
+		return new Promise((resolve, reject) => {
+			if (![...appDatabase.db.objectStoreNames].includes("customers")) {
+				reject("Something went wrong 💥 no customers");
+			}
+			const transaction = appDatabase.db.transaction(
+				appDatabase.db.objectStoreNames, // INFO cannot be empty
+				"readwrite"
+			);
+			const clearRequest = transaction.objectStore("customers").clear();
+			clearRequest.onerror = (err) => reject(err);
+			clearRequest.onsuccess = (event) => resolve(event);
+		});
+	},
+
+	/**
+	 * Get customers from db table `customers` returning all of them in a Promise
+	 */
+	getCustomers() {
+		return new Promise((resolve, reject) => {
+			if (!appDatabase.db) {
+				reject("Database not instatiated");
+			}
+			const transaction = appDatabase.db.transaction("customers"); // read-only by default
+			transaction.onerror = (err) => reject("Transaction error ❌", err);
+			const getRequest = transaction.objectStore("customers").getAll();
+			getRequest.onerror = (err) => reject("getAll() error ❌", err);
+			getRequest.onsuccess = (event) => resolve(event.target.result);
+		});
+	},
+
+	/**
+	 * Add customer object to database
+	 *
+	 * @param {any} customer
+	 */
+	addCustomer(customer) {
+		return new Promise((resolve, reject) => {
+			const addCustomerTransaction = appDatabase.db.transaction("customers", "readwrite");
+			addCustomerTransaction.onerror = (err) => {
+				reject("Transaction error ❌", err);
+			};
+
+			const customerStore = addCustomerTransaction.objectStore("customers");
+			const addRequest = customerStore.add(customer);
+			addRequest.onerror = (err) => {
+				reject("getAll() error ❌", err);
+			};
+			addRequest.onsuccess = (_event) => {
+				resolve(`Customer added to database ✅`);
+			};
+		});
+	},
 };
 
 // try to connect db of certain version, creates one if doesn't exist
-customerDatabase.getDb();
+appDatabase.connectDb().then(refresh);
+
+// reset db button
+document.querySelector("#reset").addEventListener("click", async () => {
+	await appDatabase.clearDb();
+	refresh();
+});
+
+// add customer to page
+document.querySelector("#add").addEventListener("click", async () => {
+	await appDatabase.addCustomer(startData[Math.floor(Math.random()*startData.length)]);
+	refresh();
+});
 
 /**
- * Reset db
+ * Refresh the page displaying customer information
  */
-function resetDb() {
-  console.log("Resetting DB ⏱");
-  const reset = customerDatabase.db.transaction(
-    customerDatabase.db.objectStoreNames,
-    "readwrite"
-  );
-  reset.oncomplete = async (event) => {
-    console.log("Db reset connection made 🔁", event);
-    if (![...customerDatabase.db.objectStoreNames].includes("customers")) {
-      console.error("Something went wrong 💥 no customers");
-      return;
-    }
-    const customersStore = customerDatabase.db
-      .transaction("customers", "readwrite")
-      .objectStore("customers");
-    console.log("Creating customers 🧑👩");
-    for (const customer of customerDatabase.startData) {
-      console.log("🧑 Adding ", customer);
-      await customersStore.add(customer);
-    }
-  };
+async function refresh() {
+	const customerTable = document.querySelector("#customerTable");
+	const customers = await appDatabase.getCustomers();
+	if (customers.length > 0)
+		customerTable.innerHTML = `<table>
+      <thead>
+          <tr>
+              <th>ssn</th>
+              <th>name</th>
+              <th>age</th>
+              <th>email</th>
+          </tr>
+      </thead>
+      <tbody>
+          ${customers
+					.map(
+						(customer) => `
+          <tr>
+              <th>${customer.ssn}</th>
+              <th>${customer.name}</th>
+              <th>${customer.age}</th>
+              <th>${customer.email}</th>
+          </tr>
+          `
+					)
+					.join("")}
+      </tbody>
+  </table>`;
+	else customerTable.innerHTML = `No customers so far`;
 }
-document.querySelector("#reset").addEventListener("click", resetDb);
-
-/**
- * Get customers from db
- */
-function getCustomers() {
-  if (!customerDatabase.db) {
-    return;
-  }
-  console.log("Requesting Customers...");
-  const transaction = customerDatabase.db.transaction("customers"); // read-only by default
-  transaction.onerror = (err) => {
-    console.error("Transaction error ❌", err);
-  };
-
-  const curStore = transaction.objectStore("customers");
-  const getRequest = curStore.getAll();
-  getRequest.onerror = (err) => {
-    console.error("getAll() error ❌", err);
-  };
-
-  getRequest.onsuccess = (event) => {
-    console.log("Customers Retrieved. 👩👨🧑");
-    console.table(event.target.result);
-    document.querySelector("#customerTable").innerHTML = `<table>
-                <thead>
-                    <tr>
-                        <th>ssn</th>
-                        <th>name</th>
-                        <th>age</th>
-                        <th>email</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    ${event.target.result
-                      .map(
-                        (customer) => `
-                    <tr>
-                        <th>${customer.ssn}</th>
-                        <th>${customer.name}</th>
-                        <th>${customer.age}</th>
-                        <th>${customer.email}</th>
-                    </tr>
-                    `
-                      )
-                      .join("")}
-                </tbody>
-            </table>`;
-  };
-}
-// load customers to page
-document.querySelector("#get").addEventListener("click", getCustomers);
+document.querySelector("#get").addEventListener("click", refresh);
